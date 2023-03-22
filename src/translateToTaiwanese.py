c3c6c0d27dd45a5a8f1a1602619c0a3ec631923f
atamakonkurii <morisudesu@gmail.com>
2023-03-23 08:49:54 +0900

    ファイルを読み込んで翻訳をするのが目標

---
+++ src/translateToTaiwanese.py
@@ -0,0 +1,8 @@
+# transcriptJapanese.srtファイルをdeepLで翻訳
+
+# srtファイルを読み込む
+srt_file = open("./output/transcriptJapanese.srt", "r")
+
+# srtファイルを読み込んで、リストにする
+srt_list = srt_file.readlines()
+
--- src/whisper.py
+++ src/whisper.py
@@ -11,6 +11,14 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 # audioファイル読み込み
 audio_file= open("./audio_file/test.mp3", "rb")
 
-# 
-transcript = openai.Audio.transcribe("whisper-1", audio_file)
-print(transcript.text)
+# whisper-1モデルで音声認識⇨Japanese
+transcriptJapanese = openai.Audio.transcribe(model="whisper-1", 
+                                     file=audio_file,
+                                     language="ja",
+                                     temperature=0,
+                                     response_format="srt",)
+
+with open('./output/transcriptJapanese.srt', 'w') as f:
+  print(transcriptJapanese, file=f)
+
+
