dc9f5ac643f5c7df33f5f4a1ab80fe5a67cc5f9c
atamakonkurii <morisudesu@gmail.com>
2023-03-24 22:27:29 +0900

    任意のファイル名でも動画を読み込めるようにした

--- src/convert/mp4_to_mp3.py
+++ src/convert/mp4_to_mp3.py
@@ -1,7 +1,21 @@
-import ffmpeg 
+import ffmpeg
+import os
+
+def find_first_mp4_file(directory):
+    for file in os.listdir(directory):
+        if file.lower().endswith('.mp4'):
+            return os.path.join(directory, file)
+    return None
+
+input_directory = "./input_movie"
+input_file = find_first_mp4_file(input_directory)
+
+if input_file is None:
+    print(f"No MP4 files found in {input_directory}")
+    sys.exit(1)
 
 # 入力 
-stream = ffmpeg.input("./input_movie/movie.mp4") 
+stream = ffmpeg.input(input_file) 
 # 出力 
 stream = ffmpeg.output(stream, "./output/mp3/voice.mp3") 
 # 実行 
